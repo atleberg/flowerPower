2eefcfa841e0396a2aefcb046ee6db103e5fce00
atleberg <admin@atleberg.com>
2015-12-21 14:19:25 +0100

    Added skeleton setup for python

---
+++ flowerPower/flowerPower.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# flowerPower main Python app
+# Copyright 2015 Atle Krogstad Berg (mail@atleberg.com)
+#
+
+import os
+import sys
+import time
+import logging
+import ConfigParser
+
+__version__ = 0.1
+__author__ = 'Atle Krogstad Berg - mail@atleberg.com'
+
+
+
+## APPLICATION ##
+
+def main():
+	currentPath = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+
+if (__name__ == "__main__"):
+	main()
+
